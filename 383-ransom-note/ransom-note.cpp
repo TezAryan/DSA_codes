ab54b05a4fb9c0c0c1bc146fdbe3ebf1edeaf68a
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-03-18 00:09:05 +0530

    Time: 3 ms (55.50%) | Memory: 12 MB (10.71%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> have;
+        unordered_map<char , int> need;
+
+        for(int i = 0 ; i<ransomNote.size() ; i++) {
+            need[ransomNote[i]]++;
+        } for(int i =0; i<magazine.size() ; i++) {
+            have[magazine[i]]++;
+        }
+
+        return func(have , need);
+    }
+
+    bool func(unordered_map<char,int> &have , unordered_map<char,int> &need) {
+        
+        for(auto i : need) {
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c]; 
+            
+            if( fhave < fneed ) return false;
+
+    
+         }
+
+         return true;
+    }
+    
+};
