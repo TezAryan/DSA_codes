7017b0398d9651179ac21b4ca076530978f6e104
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-03-19 15:22:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (83.53%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,29 +1,48 @@
+// class Solution {
+// public:
+//     int maxNumberOfBalloons(string text) {
+//         int n = text.size();
+//         unordered_map<char, int> have;
+//         for (char c : text) {
+//             have[c]++;
+//         }
+//         unordered_map<char, int> need;
+//         need['b'] = 1;
+//         need['a'] = 1;
+//         need['l'] = 2;
+//         need['o'] = 2;
+//         need['n'] = 1;
+
+//         int res = INT_MAX;
+
+//         for(auto i : need) {
+//             char c = i.first;
+//             int fneed = i.second;
+//             int fhave = have[c];
+//             int times = fhave / fneed;
+
+//             res= min(res,times);
+//         }
+
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int n = text.size();
-        unordered_map<char, int> have;
+        vector<int> freq(26, 0);
+
         for (char c : text) {
-            have[c]++;
-        }
-        unordered_map<char, int> need;
-        need['b'] = 1;
-        need['a'] = 1;
-        need['l'] = 2;
-        need['o'] = 2;
-        need['n'] = 1;
-
-        int res = INT_MAX;
-
-        for(auto i : need) {
-            char c = i.first;
-            int fneed = i.second;
-            int fhave = have[c];
-            int times = fhave / fneed;
-
-            res= min(res,times);
+            freq[c - 'a']++;
         }
 
-        return res;
+        return min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['l' - 'a'] / 2,
+            freq['o' - 'a'] / 2,
+            freq['n' - 'a']
+        });
     }
 };
