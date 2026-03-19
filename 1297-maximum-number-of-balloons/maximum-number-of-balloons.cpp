c1738d2f85a30d48560ba147c620030488f8633f
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-03-19 15:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (62.89%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int n = text.size();
+        unordered_map<char, int> have;
+        for (char c : text) {
+            have[c]++;
+        }
+        unordered_map<char, int> need;
+        need['b'] = 1;
+        need['a'] = 1;
+        need['l'] = 2;
+        need['o'] = 2;
+        need['n'] = 1;
+
+        int res = INT_MAX;
+
+        for(auto i : need) {
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c];
+            int times = fhave / fneed;
+
+            res= min(res,times);
+        }
+
+        return res;
+    }
+};
