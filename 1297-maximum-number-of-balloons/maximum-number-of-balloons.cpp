8efb1fa5a822616dfe7bf1f824825b8321f92ca4
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-03-23 01:58:36 +0530

    Time: 3 ms (13.93%) | Memory: 9 MB (38.26%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,54 +1,54 @@
-class Solution {
-public:
-    int maxNumberOfBalloons(string text) {
-        int n = text.size();
-
-        unordered_map<char, int> have;
-        for (char c : text) {
-            have[c]++;
-        }
-        unordered_map<char, int> need;
-        need['b'] = 1;
-        need['a'] = 1;
-        need['l'] = 2;
-        need['o'] = 2;
-        need['n'] = 1;
-
-        int res = INT_MAX;
-
-        for(auto i : need) {
-            char c = i.first;
-            int fneed = i.second;
-            int fhave = have[c];
-            int times = fhave / fneed;
-
-            res= min(res,times);
-        }
-
-        return res;
-    }
-};
-
-
-
 // class Solution {
 // public:
 //     int maxNumberOfBalloons(string text) {
-//         vector<int> freq(26, 0);
+//         int n = text.size();
 
+//         unordered_map<char, int> have;
 //         for (char c : text) {
-//             freq[c - 'a']++;
+//             have[c]++;
+//         }
+//         unordered_map<char, int> need;
+//         need['b'] = 1;
+//         need['a'] = 1;
+//         need['l'] = 2;
+//         need['o'] = 2;
+//         need['n'] = 1;
+
+//         int res = INT_MAX;
+
+//         for(auto i : need) {
+//             char c = i.first;
+//             int fneed = i.second;
+//             int fhave = have[c];
+//             int times = fhave / fneed;
+
+//             res= min(res,times);
 //         }
 
-//         return min({
-//             freq['b' - 'a'],
-//             freq['a' - 'a'],
-//             freq['l' - 'a'] / 2,
-//             freq['o' - 'a'] / 2,
-//             freq['n' - 'a']
-//         });
+//         return res;
 //     }
-// // };
+// };
+
+
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> freq(26, 0);
+
+        for (char c : text) {
+            freq[c - 'a']++;
+        }
+
+        return min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['l' - 'a'] / 2,
+            freq['o' - 'a'] / 2,
+            freq['n' - 'a']
+        });
+    }
+};
 
 // class Solution {
 // public:
