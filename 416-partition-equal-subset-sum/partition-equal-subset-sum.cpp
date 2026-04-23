1f516927a7f48ddd5a6bf5cfc2a4aa7906cfbb52
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-04-23 14:15:02 +0530

    Time: 79 ms (80.19%) | Memory: 13.4 MB (92.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,34 +1,64 @@
+// class Solution {
+// public:
+//  /// 2d dp method
+//     bool subsetSum(vector<int>& nums , int n , int sum) {
+//         vector<vector<bool>>t(n+1 , vector<bool>(sum+1 , false));
+
+//         for(int i = 0 ; i<= n ; i++) {
+//             t[i][0] = true;
+//         }
+//         for( int i = 1 ; i<= n ; i++) {
+//             for(int j = 1 ; j<= sum ; j++) {
+//                 if(nums[i-1] <= j) {
+//                     t[i][j] = t[i-1][j - nums[i-1]] || t[i-1][j];
+//                 } else {
+//                     t[i][j] = t[i-1][j];
+//                 }
+//             }
+//         }
+//         return t[n][sum];
+
+//     }
+
+//     bool canPartition(vector<int>& nums) {
+//         int n = nums.size();
+//         int sum = 0;
+//         for(int i = 0 ; i<n ; i++) {
+//             sum += nums[i];
+//         }
+
+//         if(sum % 2 != 0) return false;
+//         else
+//         return subsetSum(nums , n , sum/2);
+//     }
+// };
+
 class Solution {
 public:
-
-    bool subsetSum(vector<int>& nums , int n , int sum) {
-        vector<vector<bool>>t(n+1 , vector<bool>(sum+1 , false));
-
-        for(int i = 0 ; i<= n ; i++) {
-            t[i][0] = true;
-        }
-        for( int i = 1 ; i<= n ; i++) {
-            for(int j = 1 ; j<= sum ; j++) {
-                if(nums[i-1] <= j) {
-                    t[i][j] = t[i-1][j - nums[i-1]] || t[i-1][j];
-                } else { 
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        return t[n][sum];
-
-    }
+    /// 2d dp method
 
     bool canPartition(vector<int>& nums) {
+
         int n = nums.size();
         int sum = 0;
-        for(int i = 0 ; i<n ; i++) {
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
         }
 
-        if(sum % 2 != 0) return false;
-        else 
-        return subsetSum(nums , n , sum/2);
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+
+        dp[0] = true;
+
+        for(int i = 0 ; i<n ; i++) {
+            for(int j = target ; j>= nums[i] ; j--) {
+                dp[j] =  dp[j] || dp[j-nums[i]] ;
+
+            }
+        }
+        return dp[target];
     }
 };
