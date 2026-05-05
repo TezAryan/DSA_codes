771edcc2f2e55a65cf2ca92c80c907d34ee47bbf
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-05-06 00:35:46 +0530

    Time: 14 ms (21.64%) | Memory: 7.9 MB (38.17%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+
+    int fib(int n) {
+
+        if( n==0) return 0;
+        if (n==1) return 1;
+
+        int prev = fib(n-1);
+        int prev1 = fib(n-2);
+
+        return prev + prev1;
+        
+    }
+};
