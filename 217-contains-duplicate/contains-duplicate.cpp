5bd33ac88a3489497c958b7b5299b5f9836b53d4
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-06-25 12:08:55 +0530

    Time: 66 ms (17.30%) | Memory: 92.9 MB (12.93%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int,int> mp; 
+
+        for(auto it : nums ) { 
+            mp[it]++; 
+        }
+
+        for(auto i : mp ) {
+            if( i.second > 1) 
+            return true;
+        } 
+
+        return false;
+    }
+};
