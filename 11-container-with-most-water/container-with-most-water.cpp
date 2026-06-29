e4f0605da19d4205b17c6b7c87b97bd9e2b95d20
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-06-29 18:00:57 +0530

    Time: 8 ms (6.92%) | Memory: 63 MB (10.71%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
-        int area = 0;
-        int ans = INT_MIN;
-        int n = height.size();
-        int l = 0 ;
-        int r = n-1;
-        while ( l < r ) {
-           int area = min(height[l] , height[r]) * (r-l);
-         ans = max(area , ans);
-            if(height[l] < height[r]) l++;
-            else r--;
+        int area = INT_MIN ; 
+        int n = height.size(); 
+        int l = 0 ; 
+        int r = n -1 ; 
+        int ans = 0; 
+        while ( l < r) {
+
+            area = min(height[l] , height[r]) * (r-l);
+            ans = max( ans , area); 
+
+            if ( height[l] < height[r]) l++; 
+            else 
+            r--;
+
         }
 
         return ans;
-    }
+     }
 };
