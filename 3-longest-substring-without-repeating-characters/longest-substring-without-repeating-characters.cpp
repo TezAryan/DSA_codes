43fd8dd67285c29b35679e7c3234fb4709b7eb4d
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-07-15 22:52:23 +0530

    Time: 15 ms (36.33%) | Memory: 14.3 MB (20.24%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -9,16 +9,17 @@ public:
 
         for (int high = 0; high < s.size(); high++) {
 
-            while (mp.find(s[high]) != mp.end()) {
+            while (mp.find(s[high]) != mp.end()) { 
                 mp.erase(s[low]);
                 low++;
             }
 
             mp[s[high]] = 1;
 
-            res = max(res, high - low + 1);
+            res = max(res ,  high - low + 1);
+        
         }
 
         return res;
-    }
-};
+        }
+    };
