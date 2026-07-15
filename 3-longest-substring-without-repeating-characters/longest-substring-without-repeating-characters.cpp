c572fffc29ec389d90f6801eab0030de2a3fa448
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-07-15 23:00:11 +0530

    Time: 8 ms (56.24%) | Memory: 12.3 MB (40.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,57 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+
+//         unordered_map<char, int> mp;
+
+//         int low = 0;
+//         int res = 0;
+
+//         for (int high = 0; high < s.size(); high++) {
+
+//             while (mp.find(s[high]) != mp.end()) { 
+//                 mp.erase(s[low]);
+//                 low++;
+//             }
+
+//             mp[s[high]] = 1;
+
+//             res = max(res ,  high - low + 1);
+        
+//         }
+
+//         return res;
+//         }
+//     };
+
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
-        unordered_map<char, int> mp;
+        unordered_map<char,int> mp;
 
         int low = 0;
-        int res = 0;
+        int ans = 0;
 
-        for (int high = 0; high < s.size(); high++) {
+        for(int high = 0 ; high < s.size() ; high++) { 
 
-            while (mp.find(s[high]) != mp.end()) { 
+            mp[s[high]]++; 
+
+            while ( mp[s[high]] > 1) { 
+                mp[s[low]]--;
+
+                if(mp[s[low]] == 0 ) 
                 mp.erase(s[low]);
+
                 low++;
             }
 
-            mp[s[high]] = 1;
-
-            res = max(res ,  high - low + 1);
-        
+            ans = max(ans , high-low+1);
         }
 
-        return res;
-        }
-    };
+        return ans;
+       
+    }
+};
