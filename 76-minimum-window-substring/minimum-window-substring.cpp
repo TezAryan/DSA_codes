f6b6755fa091e1e7639a880dc87d919c0e6a9c67
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-07-22 22:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (26.59%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,41 +1,58 @@
 class Solution {
 public:
-    bool fun(vector<int> &have , vector<int> &need){
-        for(int i =0; i<256 ; i++) {
-            if(have[i] < need[i])
-            return false;
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m =t.size();
+        
+        vector<int> need ( 128 , 0); 
+        vector<int> window(128 , 0); 
 
-        vector<int> have(256,0);
-        vector<int> need(256,0);
-        int i;
-        if(n<m)
-        return "";
-        for(i = 0 ; i<m;i++) 
-        need[t[i]]++;
-
-        int low=0 , high = 0;
-        int res = INT_MAX;
-        int start = -1;
-        for(high =0; high <n; high++){
-            have[s[high]]++;
-            while(fun(have,need)){
-                int len=high-low+1;
-                if(res>len){
-                    res=len;
-                    start=low;
-                }
-                have[s[low]]--;
-                low++;
-            }
+        for(char c : t) {
+            need[c]++;
         }
-        if(res==INT_MAX)
-        return "";
-        return s.substr(start,res);
+
+        int needCount = 0; 
+
+        for(int i =0 ; i< 128 ; i++) { 
+            if(need[i] > 0) 
+            needCount++;
+        }
+
+        int left = 0; 
+        int minLen = INT_MAX; 
+        int start = 0; 
+        int have = 0;
+
+        for(int right = 0 ; right < s.size() ; right++) { 
+
+            window[s[right]]++; 
+
+        if( need[s[right]] > 0 && window[s[right]] == need[s[right]]) { 
+            have++;
+        }
+
+        while( have == needCount ) { 
+            
+
+            if(right-left+1 < minLen) {
+                minLen = right - left + 1; 
+                start = left;
+            }
+
+            window[s[left]]--; 
+
+            if( need[s[left]] > 0 && window[s[left]] < need[s[left]])
+            have--;
+
+
+            left++;
+
+
+        }
+
+
+        }
+
+        if(minLen == INT_MAX) return "";
+
+        return s.substr(start , minLen);
     }
 };
