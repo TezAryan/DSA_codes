02140d1f77e6797b8a847bfe9b5a742af8390f64
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-07-23 21:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (22.70%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& interval, vector<int>& newInterval) {
-        int nstart = newInterval[0];
-        int nend = newInterval[1];
-        int n = interval.size();
-        int i = 0;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
 
-
+        int nStart = newInterval[0];
+        int nEnd = newInterval[1];
         vector<vector<int>> result;
-        while ( i < n && interval[i][1] < nstart ) {
-            result.push_back(interval[i]);
+        int n = intervals.size();
+        int i = 0; 
+
+        while(i < n && intervals[i][1] < nStart) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        while ( i < n && intervals[i][0] <= nEnd) {
+            nStart = min(intervals[i][0] , nStart);
+            nEnd = max(intervals[i][1] , nEnd);
             i++;
         }
         
-        while ( i < n && interval[i][0] <= nend){
-            nstart = min(nstart , interval[i][0]);
-            nend = max(nend , interval[i][1]);
+         result.push_back({nStart , nEnd});
+
+        while ( i < n) { 
+
+            result.push_back(intervals[i]);
             i++;
+
         }
 
-        result.push_back({nstart , nend}); 
-
-        while ( i < n ) { 
-            result.push_back(interval[i]);
-            i++;
-        }
 
         return result;
-
-       
     }
 };
