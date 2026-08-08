9cf90695eb09ae714845e97545e3b7cbf4ec97b1
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-08-08 15:23:31 +0530

    Time: 4 ms (24.12%) | Memory: 13.5 MB (36.50%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,54 +1,56 @@
 class Solution {
 public:
-    vector<int> parent; 
 
-    int find(int node) { 
-        if(parent[node] == node) { 
-            return node; 
+
+        vector<int> parent; 
+
+        int find( int node) { 
+            if( parent[node] == node) return node; 
+
+            return parent[node] = find(parent[node]);
+
         }
 
-        return parent[node] = find(parent[node]);
-    }
+        void Union( int u , int v) { 
 
+            int pu = find(u);
+            int pv = find(v); 
 
-    void Union( int u , int v) {
+            if( pu != pv) { 
+                parent[pv] = pu;
+            }
+        }
 
-        int pu = find(u);
-        int pv = find(v);
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
 
-
-
-
-        if( pu != pv) 
-        parent[pv] = pu; 
-    }
-
-
-     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-
-        int n = edges.size();
-
+        int n = edges.size(); 
+        
         parent.resize(n+1);
 
-        for( int i = 0 ; i<= n ; i++) { 
+        for( int i =0 ; i<=n ; i++) { 
             parent[i] = i;
         }
 
 
-        for(auto it : edges) { 
+        for(auto it: edges) { 
             int u = it[0];
-            int v = it[1];
+            int v = it[1]; 
 
 
-            if(find(u) == find(v))
-            return it;
+            if( find(u) == find(v)) { 
+                return it;
+            }
+
 
             Union( u , v);
-
-
         }
 
-        return {};
-        
+
+
+
+            return {};
+
+
+
     }
 };
