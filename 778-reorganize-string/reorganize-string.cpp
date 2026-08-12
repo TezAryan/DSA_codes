ea11ea4f7097bce6687e05640c58c09bf589dba0
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-08-12 22:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (30.19%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,40 +1,59 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        
-        unordered_map<char , int> f;
-        for( char c : s) {
-            f[c]++;
+
+        unordered_map<char, int> mp;
+
+        int n = s.size();
+
+        for (auto it : s) {
+            mp[it]++;
         }
 
-        int n = s.length();
-        for(auto &it : f) {
-            if (it.second > (n+1) / 2 ) return "";
+        priority_queue < pair<int, char>> pq;
+
+        for (auto it : mp) {
+            pq.push({it.second, it.first});
         }
 
-        priority_queue<pair<int , char>> pq;
-        for(auto it : f) {
-            pq.push({it.second , it.first});
+            string ans = "";
+
+        while( !pq.empty()) { 
+
+            auto first = pq.top();
+            pq.pop(); 
+
+            if(!pq.empty()) { 
+                auto second = pq.top();
+                pq.pop(); 
+            
+
+            ans+= first.second;
+            ans+=second.second;
+
+            first.first--;
+            second.first--;
+
+
+            if( first.first > 0) {
+                pq.push(first);
+            }
+
+            if(second.first >0 ) { 
+                pq.push(second);
+            }
+
+
+            } else { 
+                if(first.first > 1) 
+                return "";
+
+                ans+= first.second;
+            }
+               
         }
 
+        return ans;
 
-
-        string result = "";
-        pair<int , char> prev =  { 0 , '#' };
-
-        while(!pq.empty()) { 
-         auto curr = pq.top();
-            pq.pop();
-
-            result += curr.second;
-            curr.first--;
-
-            if(prev.first > 0 )
-            pq.push(prev);
-
-            prev = curr;
-        }
-
-        return result.length() == s.length() ? result : "";
     }
 };
