5f4db4e78f86ae12bac97c1d2223b3739a60c93f
TezAryan <165379888+TezAryan@users.noreply.github.com>
2026-08-21 00:55:55 +0530

    Time: 6 ms (89.53%) | Memory: 10.3 MB (86.52%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+
+        vector<long long> dp(amount + 1, 0);
+
+        dp[0] = 1;
+
+        for(int coin : coins) {
+
+            for(int i = coin; i <= amount; i++) {
+
+                dp[i] = min(dp[i] + dp[i - coin],
+                            (long long)INT_MAX);
+            }
+        }
+
+        return dp[amount];
+    }
+};
